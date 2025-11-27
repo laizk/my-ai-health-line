2f70c0ef9d81f5da8be8b30154f93c5b0435b568
t0pZ <kristoffer.laiz@outlook.com>
2025-11-27 17:49:21 +0800

    Link frontend to backend i.e. use llm

--- apps/frontend/pages/patient.py
+++ apps/frontend/pages/patient.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import requests
+import json
+
+
+BACKEND_API_URL = "http://backend:8010/ask"
 
 st.title("Patient")
 
@@ -14,8 +19,49 @@ for message in st.session_state.messages:
         
 # React to user input
 if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})        
+    
+        try:
+            
+            # Display user message in chat message container
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})   
+            
+            # Parse JSON
+            payload = {
+                "prompt": prompt
+            }
+
+            # Send to FastAPI endpoint
+            response = requests.post(
+                BACKEND_API_URL,
+                json=payload,
+                timeout=8
+            )            
+            
+            data = response.json()
+            response_text = (
+                data.get("response", [{}])[0]
+                    .get("content", {})
+                    .get("parts", [{}])[0]
+                    .get("text", "")
+)
+            print('response_text', response_text)
+            
+            # response = f"Echo: {prompt}"
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                st.markdown(response_text)
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": response_text})                
+
+        except json.JSONDecodeError as e:
+            st.error("Invalid JSON format.")
+            st.code(str(e))
+
+        except Exception as e:
+            st.error("Failed to send request.")
+            st.exception(e)    
+
+    
